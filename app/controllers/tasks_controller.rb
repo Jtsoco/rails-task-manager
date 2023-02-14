c003f0fd65bb358dfc4e547703fa55c384f4232a
Jackson Socolofsky <jtsoco@gmail.com>
2023-02-14 14:15:28 +0900

    finished required, optionals incomplete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,10 +33,24 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  def complete
+    @task = Task.find(params[:id])
+    @task.completed = true
+    @task.save
+    redirect_to task_path(@task), status: :see_other
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
   end
+
 
 end
